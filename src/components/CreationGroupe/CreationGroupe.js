62af077f6fea1821b8d9725cf0e51fc643df147d
Yohann LE-CAM <yohann.le-cam@isen-ouest.yncrea.fr>
2023-02-17 19:08:14 +0100

    Requete de création d'un groupe (en brut)

--- src/components/CreationGroupe/CreationGroupe.js
+++ src/components/CreationGroupe/CreationGroupe.js
@@ -94,7 +94,25 @@ function CreationGroupe(props){
 
     //fonction qui envoie la requete api pour créer le groupe
     function handleCreateGroupe() {
-        
+        const url = 'http://127.0.0.1:8000/api/v1.0/groupe/creation';
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(
+            {
+                nom: "Test5" 
+            }
+          )
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
     }
 
 
