4a16c0acc2a0f5d1d787a90fbbbc20abedf22569
Clement YZIQUEL <clement.yziquel@isen-ouest.yncrea.fr>
2023-02-27 17:35:01 +0100

    CAS auth, début

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect,useState, useContext, Box, Text, Spinner, Button, Paragraph,Login,Logout} from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, Navigate, useNavigate, Outlet} from 'react-router-dom';
 import Header from './components/Header/Header.js';
 import Filtres from './components/Filtres/Filtres.js';
@@ -6,38 +6,131 @@ import Listes from './components/Listes/Listes.js';
 import CreationSession from './components/CreationSession/CreationSession.js';
 import CreationGroupe from './components/CreationGroupe/CreationGroupe.js';
 import Connexion from './components/Connexion/Connexion.js';
-import { CasUserContextProvider, useCasUserContext } from './context/casUserContext';
+import { CasUserContextProvider, useCasUserContext, CasUserContext } from './context/casUserContext';
+import useCas from './hooks/useCas.js';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user , setUser] = useState(null);
+  return (
+      <Router>
+          
+          <Routes>
+            {/* <Route path='/connexion' element={<Connexion/>} />  */}
+            <Route path="/sessions" element={<Sessions />} />
+            <Route path="/groupes" element={<Groupes />} />
+            <Route path="/creation" element={<Creation />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+      </Router>
+  );
+}
 
-  const handleLogin = () => setUser({id: '1', identifiant: 'cyziqu24', isAdmin: false });
-  const handleLogout = () => setUser(null);
-  
+function Layout(props){
+  const navigate = useNavigate();
+  const [securityChecked, setSecurityChecked] = useState(false);
+  const casUserContext = useContext(CasUserContext);
 
-  function PrivateRoute({ element, ...rest }) {
-    const { is } = useCasUserContext();
-    return isAuthenticated ? 
-      (<Outlet />) : (<Navigate to="/connexion" replace />);
-  }
+  useEffect(() => {
+    if (props.isSecure && !casUserContext.user) {
+      navigate('/');
+    }else{
+      setSecurityChecked(true);
+    }
+  }, [props.isSecure,casUserContext.user]);
 
   return (
-    <CasUserContextProvider>
-      <Router>
-          <Routes>
-            <Route element={<PrivateRoute user={user}></PrivateRoute>}>
-              <Route element={<Header />} />
-              <Route path="/sessions" element={<Sessions />} />
-              <Route path="/groupes" element={<Groupes />} />
-              <Route path="/creation" element={<Creation />} />
-            </Route>
+    <Connexion/>
+  );
+}
 
-            <Route path='/connexion' element={<Connexion setIsAuthenticated={setIsAuthenticated} /> } /> 
-            <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Router>
-    </CasUserContextProvider>
+function Sessions(){
+  const navigate = useNavigate();
+  const cas = useCas(true);
+  const casUserContext = useContext(CasUserContext);
+
+  useEffect(() => {
+    if (casUserContext.user) {
+      navigate('/sessions');
+    }
+  }, [casUserContext.user]);
+
+  return (
+    <Layout background="status-unknown" isSecure={false}>
+      {cas.isLoading && (
+        <Box align="center" gap="medium">
+          <Spinner
+            border={[
+              {
+                side: "all",
+                color: "brand",
+                size: "medium",
+                style: "dotted",
+              },
+            ]}
+          />
+          <Text>Redirecting ...</Text>
+        </Box>
+      )}
+      {!cas.isLoading && (
+        <Box align="center" gap="xsmall">
+          <Paragraph textAlign="center">
+            Hello anonymous! Please click{" "}
+            <Text color="brand" size="large">
+              LOGIN
+            </Text>{" "}
+            with follows account.
+          </Paragraph>
+          <Box align="center" gap="xsmall" direction="column">
+            <Text size="xsmall">Username / Password</Text>
+            <Text size="large">
+              <Text color="brand" size="large">
+                casuser
+              </Text>{" "}
+              /{" "}
+              <Text color="brand" size="large">
+                Mellon
+              </Text>
+            </Text>
+          </Box>
+          <Button
+            label="Login"
+            icon={<Login />}
+            a11yTitle="Login button"
+            margin="medium"
+            reverse
+            onClick={() => {
+              cas.attemptCasLogin(false);
+            }}
+          />
+        </Box>
+      )}
+    </Layout>
+  );
+}
+
+function SecureHome(){
+  const cas = useCas();
+  const casUserContext = useContext(CasUserContext);
+
+  return (
+    <Layout background="status-ok" isSecure={true}>
+      <Box align="center" gap="medium">
+        <Paragraph textAlign="center">
+          welome{" "}
+          <Text color="brand" size="xxlarge">
+            {casUserContext.user}
+          </Text>
+        </Paragraph>
+      </Box>
+      <Button
+        label="Logout"
+        icon={<Logout />}
+        a11yTitle="Logout button"
+        reverse
+        onClick={() => {
+          cas.logout();
+        }}
+      />
+    </Layout>
   );
 }
 
@@ -54,12 +147,12 @@ const Home = () => (
   </div>
 );
 
-const Sessions = () => (
-  <div>
-    <Filtres />
-    <Listes />
-  </div>
-);
+// const Sessions = () => (
+//   <div>
+//     <Filtres />
+//     <Listes />
+//   </div>
+// );
 const Groupes = () => (
   <div>
     <Filtres />
