04574c9aefaa8183bcb51b3aead65170b4777a3f
yohannlc <yohann.lecamx@gmail.com>
2023-02-28 16:22:50 +0100

    Css du select

--- src/components/CreationSession/CreationSession.js
+++ src/components/CreationSession/CreationSession.js
@@ -8,6 +8,49 @@ import './CreationSession.css'
 
 const animatedComponents = makeAnimated();
 
+const selectStyle = {
+    control: (provided, state) => ({
+        ...provided,
+        width: '220px',
+        height: '120px',
+        backgroundColor: 'white',
+        color: 'black',
+        fontSize: '16px',
+        alignItems: 'flex-start',
+        border: '0px',
+        overflow: 'hidden',
+        ":hover": {
+            cursor: 'text',
+        }
+    }),
+    menu: (provided, state) => ({
+        ...provided,
+        width: '220px',
+        backgroundColor: 'white',
+        color: 'black',
+        fontSize: '16px',
+        margin: '0px',
+        padding: '0px',
+        ":hover": {
+            cursor: 'pointer',
+        }
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        color: 'black', // Modifier la couleur de la police
+        backgroundColor: state.isSelected ? 'blue' : 'white', // Modifier la couleur de fond en fonction de l'état de la sélection
+        '&:hover': {
+          backgroundColor: 'lightgray', // Modifier la couleur de fond au survol
+          cursor: 'pointer',
+        },
+    }),
+    valueContainer: (provided) => ({
+        ...provided,
+        width: '200px', // Modifier l'espace entre les options sélectionnées
+        padding: '3px',
+    }),
+}
+
 function CreationSession(props){
     const [groupe, setGroupe] = useState('');
     const [salle, setSalle] = useState('');
@@ -97,14 +140,43 @@ function CreationSession(props){
                     <div className="inputGroupe">
                         <label htmlFor='salle'>Salle(s)</label>
                         <Select
-                            closeMenuOnSelect={false}
                             components={animatedComponents}
                             isMulti
                             options={salle}
+                            limitTags={2}
+                            theme={(theme) => ({
+                                ...theme,
+                                colors: {
+                                    ...theme.colors,
+                                    primary25: 'lightgray',
+                                    primary: 'black',
+                                },
+                            })}
+                            styles={selectStyle}
                         />
                         
 
-                        
+                        {/* <Select
+                            options={salle}
+                            theme={(theme) => ({
+                                ...theme,
+                                colors: {
+                                    ...theme.colors,
+                                    primary25: 'lightgray',
+                                    primary: 'black',
+                                },
+                            })}
+                            styles={{
+                                control: (provided) => ({
+                                    ...provided,
+                                    width: '200px', // Modifier la taille du menu
+                                }),
+                                menu: (provided) => ({
+                                    ...provided,
+                                    width: '200px', // Modifier la largeur des options (la meme que le menu)
+                                }),
+                            }}
+                        /> */}
 
 
                     </div>
